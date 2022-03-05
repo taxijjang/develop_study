1a2ab6892df09c3cd329469d0d56ec96b4262745
taxijjang <gw9122@naver.com>
2022-03-05 14:34:04 +0900

    객체지향 프로그래밍 마무리 & 객체의 메모리를 효율적으로 관리하는 방법

---
+++ type_python/15-__slots__.py
@@ -0,0 +1,55 @@
+"""
+# 객체 내에 있는 변수들은 __dict__를 통해서 관리가 된다.
+# __slots__을 통해 변수를 관리:
+# 파이썬 인터프리터에게 통보 해당 클래스가 가지는 속성을 제한한다.
+# __dict__를 통해 관리되는 객체의 성능을 최적화한다. -> 다수의 객체 생성시 메모리 사용 공간 대폭 감소한다.
+"""
+
+
+class WithoutSlotClass:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+wos = WithoutSlotClass('amamov', 12)
+
+print(wos.__dict__)
+
+wos.__dict__['hello'] = 'world'
+
+print(wos.__dict__)
+
+
+class WithSlotClass:
+    __slots__ = ['name', 'age']
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+ws = WithSlotClass('amamove', 12)
+
+# print(ws.__dict__)
+print(ws.__slots__)
+
+# 메모리 사용량 비교
+import timeit
+
+
+def repeat(obj):
+    def inner():
+        obj.name = "amamov"
+        obj.age = 222
+        del obj.name
+        del obj.age
+
+    return inner
+
+
+use_slot_time = timeit.repeat(repeat(ws), number=99999)
+no_slot_time = timeit.repeat(repeat(wos), number=99999)
+
+print('use slot :', min(use_slot_time))
+print('no slot :', min(no_slot_time))
