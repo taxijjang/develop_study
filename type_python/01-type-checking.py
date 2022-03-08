7aced8812141f88aa1f144af0d4fc12d3cb28235
taxijjang <gw9122@naver.com>
2022-03-08 21:33:43 +0900

    mypy, pyright 설치와 사용법 & 주의사항

--- type_python/01-type-checking.py
+++ type_python/01-type-checking.py
@@ -10,7 +10,7 @@ str_var: str = 'hello world'
 float_var: float = 88.9
 bool_var: bool = True
 list_var: List[str] = ['1', '2', '3']
-tuple_var: Tuple[int] = (1, 2, 3)
+tuple_var: Tuple[int, ...] = (1, 2, 3)
 dic_var: Dict[str, int] = {'hello': 47, 'world': 10}
 
 
@@ -22,12 +22,12 @@ def type_check(obj, typer) -> None:
 
 
 def cal_add(x: int, y: int) -> int:
-    type_check(x, int)
-    type_check(y, int)
+    # type_check(x, int)
+    # type_check(y, int)
 
     return x + y
 
 
 print(cal_add(1, 3))
-print(cal_add('1', '2'))
-print(cal_add([1, 2, 3], [3, 4, 5]))
+# print(cal_add('1', '2'))
+# print(cal_add([1, 2, 3], [3, 4, 5]))
