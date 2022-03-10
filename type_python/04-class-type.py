dd7b7fcd9360d18ec6a7e1cb42d0968ecc6ca4c2
taxijjang <gw9122@naver.com>
2022-03-10 23:04:18 +0900

    Class Types 보충

--- type_python/04-class-type.py
+++ type_python/04-class-type.py
@@ -1,4 +1,5 @@
 # class types
+from typing import Optional
 
 
 class Hello:
@@ -6,11 +7,27 @@ class Hello:
         return 7
 
 
+class World:
+    pass
+
+
 hello: Hello = Hello()
+world: World = World()
 
 
-def foo(ins: Hello) -> int:
+def foo(ins: "Hello") -> int:
     return ins.world()
 
 
 print(foo(hello))
+
+
+class Node:
+    def __init__(self, data: int, next_node: Optional["Node"]):
+        self.data = data
+        self.next = next_node
+
+
+node = Node(12, None)
+
+node2 = Node(27, node)
