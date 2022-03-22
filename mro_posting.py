b81bab995cb81fbdc7114f209ed1d0ded023fe21
taxijjang <gw9122@naver.com>
2022-03-22 23:42:20 +0900

    Add mro 연습 코드

--- mro_posting.py
+++ mro_posting.py
@@ -1,10 +1,54 @@
 class A:
+    def name(self):
+        print('A')
+
+
+class B(A):
+    def name(self):
+        print('B')
+
+
+class C(A):
+    def name(self):
+        print('C')
+
+
+class D(A, B, C):
     pass
 
 
-class B:
-    pass
+d = D()
 
 
-class C(A, B):
-    pass
+d.name()
+
+print(D.__mro__)
+
+
+### super()는 각 단계에서 다음 메서드를 찾기 때문에 First에서 super() 호출시
+### Second의 __init__를 호출하게 된다.
+
+class First:
+    def __init__(self):
+        super(First, self).__init__()
+        print("first")
+
+
+class Second:
+    def __init__(self):
+        super(Second, self).__init__()
+        print("second")
+
+
+class Third(First, Second):
+    def __init__(self):
+        super(Third, self).__init__()
+        print("third")
+
+
+third = Third()
+"""
+>>> second
+>>> first
+>>> third
+"""
