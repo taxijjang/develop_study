a1d9de68dad7df16140fd3beca564fc2938fcf4f
taxijjang <gw9122@naver.com>
2022-04-04 23:17:00 +0900

    Add 파이썬 코루틴 활용

---
+++ 파이썬 동시성 프로그래밍/04-1-basic-fetcher.py
@@ -0,0 +1,25 @@
+import aiohttp
+import time
+import asyncio
+
+
+async def fetcher(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+
+async def main():
+    urls = ["https://naver.com", "https://google.com", "https://instagram.com"] * 10
+
+    async with aiohttp.ClientSession() as session:
+        result = await asyncio.gather(
+            *[fetcher(session, url) for url in urls]
+        )
+        print(result)
+
+
+if __name__ == '__main__':
+    start = time.time()
+    asyncio.run(main())
+    end = time.time()
+    print(end - start)
